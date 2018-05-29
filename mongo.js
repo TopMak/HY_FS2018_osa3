97eaae02509b52a7208514e4778a668b31b012c4
TopMak <26412430+TopMak@users.noreply.github.com>
2018-05-29 18:23:18 +0300

    Teht3.12 - Add mongo.js "test" file

---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+//Omat
+const auth = require('./auth')
+
+//Piilottaa urlin toistaiseksi...
+const dbUrl = auth.passu
+
+mongoose.connect(dbUrl)
+
+//Skeema
+const Yhteystieto = mongoose.model('Yhteystieto', {
+  name: String,
+  number: String,
+  date: Date
+})
+
+let uusiYhteystieto = null
+if(process.argv.length === 2){
+
+  console.log("Luettelossa:");
+  Yhteystieto
+  .find({})
+  .then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+
+} else {
+  //Uusi tietue
+  console.log("Lisätään " + process.argv[2] + " numero " + process.argv[3] + " luetteloon.")
+  uusiYhteystieto = new Yhteystieto({
+    name: process.argv[2],
+    number: process.argv[3],
+    date: new Date()
+  })
+
+  uusiYhteystieto
+    .save()
+    .then( response => {
+      //console.log(response, " Note saved!")
+      mongoose.connection.close()
+    })
+}
+
+// process.argv.forEach(function (val, index, array) {
+//   console.log(index + ': ' + val);
+// });
